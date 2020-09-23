8c7d08b0a6985324566c72149e632f7caea80b83
unknown <eze.rey92@gmail.com>
2020-09-23 17:49:13 -0300

    modi 17:49

--- frontend/JS/registro-datos.js
+++ frontend/JS/registro-datos.js
@@ -17,25 +17,33 @@ function capturar_datos(){
         this.mail =mail;
         
     }
+    //CAPTURO LO QUE PONENE EN EL FORMULARIO DE REGISTRO
     var capturar_usuario =document.getElementById("usuario").value;
     var capturar_pass =document.getElementById("contraseña").value;
     var capturar_mail =document.getElementById("mail").value;
 
-   
-   
-    console.log(capturar_usuario,"este es mi usuario");
-    console.log(capturar_pass, "esta es mi pass");
-    console.log(capturar_mail,"este es mi mail");
-    
+ if(!document.getElementById("usuario").value){
+       capturar_usuario = "No hubo dato";
+   }
+   if(!document.getElementById("contraseña").value){
+    capturar_pass = "No hubo dato";
+   }
+   if(!document.getElementById("mail").value){
+       capturar_mail = "No hubo dato"
+   }
+
+
+
+   //contruyo el objeto con el usuario que me dieron por el form la pass y el mail
     nuevo_usuario =new Usuario(capturar_usuario,capturar_pass,capturar_mail);
     agregar();
 }
 
 
 function agregar() {
+    // aca agrrgo el objeto al array mis usuarios
     arr_mis_usuarios.push(nuevo_usuario);
-    console.log(arr_mis_usuarios, "mi array de usuarios ");
-
+    
     document.getElementById("mi-formulario").reset();
     //nada mas creo un li y un a por cada uno de los valores del usuario
     document.getElementById("mis-usuarios-li").innerHTML += '<li>Usuario: <a href="">'+nuevo_usuario.usuario+'</a></li><li>Pass: <a href=""> '+nuevo_usuario.pass+'</a></li><li>mail: <a href="">'+nuevo_usuario.mail+' </a></li>'
